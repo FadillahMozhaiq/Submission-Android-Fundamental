71929a018d71423d6e47dc60857fc8c7b2fc5940
Fadillah Mozhaiq Ramadhani <FadillahMozhaiq>
2021-03-13 13:16:58 +0800

    Fix activity_main.xml layout

--- app/src/main/java/id/fadillah/fundamentalsubmission/ui/activity/splash/SplashActivity.kt
+++ app/src/main/java/id/fadillah/fundamentalsubmission/ui/activity/splash/SplashActivity.kt
@@ -15,7 +15,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, DetailActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         }, 2000L)
     }
