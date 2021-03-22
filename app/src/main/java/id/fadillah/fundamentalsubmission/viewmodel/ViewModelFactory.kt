291607ce56197c0c4ec0089b3ffca40c19cb9389
Fadillah Mozhaiq Ramadhani <FadillahMozhaiq>
2021-03-22 18:02:18 +0800

    Add another viewModel class

--- app/src/main/java/id/fadillah/fundamentalsubmission/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/id/fadillah/fundamentalsubmission/viewmodel/ViewModelFactory.kt
@@ -4,7 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import id.fadillah.fundamentalsubmission.domain.usecase.GithubUserUseCase
 import id.fadillah.fundamentalsubmission.ui.activity.detail.DetailViewModel
+import id.fadillah.fundamentalsubmission.ui.fragment.favorite.FavoriteViewModel
+import id.fadillah.fundamentalsubmission.ui.fragment.followers.FollowersViewModel
+import id.fadillah.fundamentalsubmission.ui.fragment.following.FollowingViewModel
 import id.fadillah.fundamentalsubmission.ui.fragment.home.HomeViewModel
+import id.fadillah.fundamentalsubmission.ui.fragment.repository.RepositoryViewModel
 import id.fadillah.fundamentalsubmission.util.Injection
 
 class ViewModelFactory(private val githubUserUseCase: GithubUserUseCase): ViewModelProvider.NewInstanceFactory() {
@@ -24,6 +28,14 @@ class ViewModelFactory(private val githubUserUseCase: GithubUserUseCase): ViewMo
                 HomeViewModel(githubUserUseCase) as T
             modelClass.isAssignableFrom(DetailViewModel::class.java) ->
                 DetailViewModel(githubUserUseCase) as T
+            modelClass.isAssignableFrom(FavoriteViewModel::class.java) ->
+                FavoriteViewModel(githubUserUseCase) as T
+            modelClass.isAssignableFrom(FollowersViewModel::class.java) ->
+                FollowersViewModel(githubUserUseCase) as T
+            modelClass.isAssignableFrom(FollowingViewModel::class.java) ->
+                FollowingViewModel(githubUserUseCase) as T
+            modelClass.isAssignableFrom(RepositoryViewModel::class.java) ->
+                RepositoryViewModel(githubUserUseCase) as T
             else -> throw Throwable("Unknown ViewModel Class ${modelClass.name}")
         }
     }
